009230258a7d39d6b8c5d9e890e990d54d258c70
JeongHoonCho <jkoon07@gmail.com>
2021-01-05 21:32:09 +0900

    ?

--- terminal.js
+++ terminal.js
@@ -1,33 +1,25 @@
-
-  
- 
 init()
 
-function init(){
-    // alert("HI")
-    // getEnter()
-    
-
+function init() {
     getEnter()
 }
 
-function getEnter(){
-    alert("Enter!!")
+function getEnter() {
 
-    let text_area = document.getElementById("textArea") 
-    
-    text_input = text_area.value
-    text_area.addEventListener('keypress',function(e){
-        if(e.key == 'Enter'){
+    let text_area = document.getElementById("textArea")
+
+    text_area.addEventListener('keypress', function (e) {
+        if (e.key == 'Enter') {
+            text_input = text_area.value
             writeInConsole(text_input)
         }
     });
 }
 
-function writeInConsole(input_txt){
+function writeInConsole(input_txt) {
 
     let console = document.getElementById("console")
     temp_html = `${input_txt}`
-                
+
     console.append(temp_html)
 }
