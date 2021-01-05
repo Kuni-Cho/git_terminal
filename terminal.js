cdeb17f54ca376e162678ec9223b910067426648
chosaihim <chosaihim@gmail.com>
2021-01-05 21:18:15 +0900

    first push

---
+++ terminal.js
@@ -0,0 +1,34 @@
+
+  
+ 
+init()
+
+function init(){
+    // alert("HI")
+    // getEnter()
+    
+
+    getEnter()
+}
+
+function getEnter(){
+    alert("Enter!!")
+
+    let text_area = document.getElementById("textArea") 
+    
+    text_input = text_area.value
+    text_area.addEventListener('keypress',function(e){
+        if(e.key == 'Enter'){
+            writeInConsole(text_input)
+        }
+    });
+}
+
+function writeInConsole(input_txt){
+
+    let console = document.getElementById("console")
+    temp_html = `<span sytle="color: white">
+                    ${input_txt}
+                </span>`
+    console.append(temp_html)
+}
